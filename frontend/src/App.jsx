4a2e60ae009b43015978d0c5726254e46d6c2610
Leelakrishna Y <sushant05072@gmail.com>
2026-01-04 16:17:29 +0530

    Create Serch filter for ViewJobs component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,20 @@ import { Navbar } from "./components/Navbar";
 import { Dashboard } from "./components/Dashboard";
 import { AddJob } from "./components/AddJob";
 import { Signin } from "./components/Signin";
-import { ViewJobs } from "./components/ViewJobs";
 
 function App() {
   return (
     <>
       <div className="dark:text-white dark:bg-black">
         <Navbar />
-        <ViewJobs/>
       </div>
 
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/add-job" element={<AddJob />} />
         <Route path="/sign-in" element={<Signin />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 }
